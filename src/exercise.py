1474c04f91a508f14386b3fdf4c2b6284e2238e4
MrSullivan <>
2021-05-17 21:54:03 +0000

    lots

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,15 @@
 def main():
     #write your code below this line
+    answer = greatest(2, 7, 9)
+    print("Greatest: " + str(answer))
+
+def greatest(number1, number2, number3):
+  if (number1 > number2) and (number1 > number3):
+    return number1
+  elif (number2 > number1 ) and (number2 > number3):
+    return number2
+  else:
+    return number3
 
 if __name__ == '__main__':
     main()
